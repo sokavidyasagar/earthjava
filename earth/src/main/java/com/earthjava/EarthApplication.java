e8857b34082632ac342f269bced2ee66b64db65a
sokavidyasagar <33341860+sokavidyasagar@users.noreply.github.com>
2022-09-12 17:59:46 -0600

    Add files via upload
    initial commits

---
+++ earth/src/main/java/com/earthjava/EarthApplication.java
@@ -0,0 +1,13 @@
+package com.earthjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthApplication.class, args);
+	}
+
+}
---
+++ earth/src/test/java/com/earthjava/EarthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
